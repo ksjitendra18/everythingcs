18ebe32c3081775cf0c66da8fa88dbb7d06d4b65
Jitendra <jitendraks1803@gmail.com>
2023-08-30 19:17:30 +0530

    dark theme and naming changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,15 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
-
 import robotsTxt from "astro-robots-txt";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), mdx(), svelte(), robotsTxt()],
+  integrations: [tailwind(), sitemap(), mdx(), svelte(), robotsTxt(), preact()],
   site: "https://everythingcs.dev",
   markdown: {
-    syntaxHighlight: "prism",
-  },
+    syntaxHighlight: "prism"
+  }
 });
