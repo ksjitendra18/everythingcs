58d1acec5d3edf85342dd1d17b37dbc62f3b40d8
Jitendra <jitendraks1803@gmail.com>
2023-11-04 20:09:10 +0530

    added flowbite

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,15 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import robotsTxt from "astro-robots-txt";
-
 import preact from "@astrojs/preact";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), mdx(), svelte(), robotsTxt(), preact()],
+  integrations: [tailwind(), sitemap(), mdx(), svelte(), robotsTxt(), preact(), partytown()],
   site: "https://everythingcs.dev",
   markdown: {
-    syntaxHighlight: "prism",
-  },
+    syntaxHighlight: "prism"
+  }
 });
