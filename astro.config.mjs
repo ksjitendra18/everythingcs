e4ec2ffdc2523e51b1069baf4c9191c0f175c63f
Jitendra <jitendraks1803@gmail.com>
2023-12-06 12:05:15 +0530

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
-
 import solid from "@astrojs/solid-js";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), mdx(), robotsTxt(), solid()],
   site: "https://everythingcs.dev",
